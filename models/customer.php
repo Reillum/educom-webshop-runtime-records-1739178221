1bf0ab067db7bdeb88ffce9cdc9ded1a51eecfeb
Justin Dang <justindang03@gmail.com>
2025-02-17 12:31:16 +0100

    Work in progress commit. Started with code

---
+++ models/customer.php
@@ -0,0 +1,24 @@
+<?php
+class customers {
+    private $id;
+    private $number;
+    private $name;
+    private $email;
+    private $password;
+    private $criteria;
+    
+}
+
+class order {
+
+}
+
+class orderLine {
+
+}
+
+class item {
+
+}
+
+?>
